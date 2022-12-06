bebe979b1f95aee8908cd15f9c3b6cb9da560453
Woong <rlqmatk@naver.com>
2022-12-06 20:26:46 +0900

    Feat: env.local 수정

--- lib/create-user.ts
+++ lib/create-user.ts
@@ -5,13 +5,16 @@ export const createUser = async (
   adult?: boolean | string,
   recentWebtoon?: []
 ) => {
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/signup`, {
-    method: 'POST',
-    body: JSON.stringify({ id, password, email, adult, recentWebtoon }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/auth/signup`,
+    {
+      method: 'POST',
+      body: JSON.stringify({ id, password, email, adult, recentWebtoon }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 
   const data = await response.json();
 
